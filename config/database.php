dd78f5dc0ae348ad3a5d70e51a23b7ff225b828f
joy-software <you@example.com>
2017-04-20 18:59:15 +0100

    composer lock update

--- config/database.php
+++ config/database.php
@@ -8,6 +8,15 @@ if (getenv('REDIS_URL')) {
     putenv('REDIS_PASSWORD='.$url['pass']);
 }
 
+if (getenv('CLEARDB_DATABASE_URL')) {
+    $url = parse_url(getenv('CLEARDB_DATABASE_URL'));
+
+    putenv('DB_HOST='.$url['host']);
+    putenv('DB_PORT='.$url['port']);
+    putenv('DB_PASSWORD='.$url['pass']);
+    putenv('DB_DATABASE='.substr($url["path"], 1));
+}
+
 return [
 
     /*
