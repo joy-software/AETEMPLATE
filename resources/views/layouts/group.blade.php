b88b3878fa49fa58c7bd2e2fa0ba447202736548
joy-software <you@example.com>
2017-04-21 18:55:46 +0100

    acceuil et notif

---
+++ resources/views/layouts/group.blade.php
@@ -0,0 +1,27 @@
+<div class="panel panel-default">
+    <div class="panel-heading">{{$groupname}}</div>
+    <div class="panel-body">
+
+        <div class="weather-category twt-category">
+            <ul>
+                <li class="active col-lg-3">
+                    <h4>{{$nbr_event}}</h4>
+                    <i class="icon_close_alt2"></i> Evènements
+                </li>
+                <li class="col-lg-3">
+                    <h4>{{$nbr_ads}}</h4>
+                    <i class="icon_check_alt2 "></i> Annonces
+                </li>
+
+                <li class="col-lg-3">
+                    <h4>{{$nbr_mem}}</h4>
+                    <i class="icon_plus_alt2 col-lg-3"></i> Demandes d'adhésion
+                </li>
+                <li>
+                    <button class="btn btn-primary btnViewGroup">Voir</button>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+</div>
